9d946519848b4c305b3c9ea3817e937f92878b63
David Horstman <davidzilla27@gmail.com>
2020-06-20 23:54:28 -0400

    Support partial matches as well as first-name matches

--- bot/cardImages.js
+++ bot/cardImages.js
@@ -2,89 +2,103 @@ const mtg = require("mtgsdk");
 const Discord = require("discord.js");
 
 function replyWithCardImage(msg) {
-  const { content, author, channel } = msg;
+  const { content, author } = msg;
   if (author.bot) return;
-  const matches = content.match(/{(.+?)}/g);
-  if (!matches) return;
-  matches
-    .map(match => titleCase(match))
-    .map(name =>
-      mtg.card.where({ name }).then(cards => {
-        let matchingCards = cards
-          .reduce((list, current) => {
-            const foundIndex = list.findIndex(n => n.name === current.name);
-            if (foundIndex === -1) {
-              return list.concat(current);
-            }
-            if (current.imageUrl) {
-              list[foundIndex] = current;
-            }
-            return list;
-          }, [])
-          .filter(card => {
-            if (card.name === name) return true;
-            const firstWord = card.name.split(" ")[0];
-            return firstWord.includes(name) && !firstWord.includes("'s");
-          });
-        //matchingCards.map(c => console.log(c.name));
-        if (matchingCards.length === 0) {
-          msg.reply(`I couldn't find a card named ${name}`);
-        } else if (matchingCards.length === 1) {
-          let card = matchingCards[0];
-          const embed = new Discord.MessageEmbed()
-            .setTitle(card.name)
-            .setImage(card.imageUrl);
-          channel.send(embed);
-        } else {
-          msg
-            .reply(
-              `I found several cards matching ${name}. Which did you mean?\n${matchingCards
-                .reduce(
-                  (list, card, idx) =>
-                    list + `${discordEmojiForNumber(idx)} ${card.name}\n`,
-                  ""
-                )
-                .slice(0, -1)}`
-            )
-            .then(async pollMessage => {
-              const filter = (reaction, user) =>
-                emojisForMultipleCardResults.includes(reaction.emoji.name) &&
-                user.id !== pollMessage.author.id;
-              const collector = pollMessage.createReactionCollector(filter, {
-                max: 1,
-                time: 1000 * 60 * 5,
-              });
-
-              Promise.all(
-                matchingCards.map((x, idx) => {
-                  const emoji = discordEmojiForNumber(idx);
-                  return emoji ? pollMessage.react(emoji) : Promise.resolve();
-                })
-              ).catch(() => console.log("poll terminated early"));
-
-              collector
-                .on("collect", r => {
-                  const idx = emojisForMultipleCardResults.findIndex(
-                    item => item === r.emoji.name
-                  );
-                  const card = matchingCards[idx];
-                  const embed = new Discord.MessageEmbed()
-                    .setTitle(card.name)
-                    .setImage(card.imageUrl);
-                  channel.send(embed);
-                })
-                .on("end", r => {
-                  pollMessage.delete();
-                });
+  const cardInserts = content.match(/{(.+?)}/g);
+  if (!cardInserts) return;
+  let partialMatches = [];
+  Promise.all(
+    cardInserts
+      .map(match => titleCase(match))
+      .map(name =>
+        mtg.card.where({ name }).then(cards => {
+          let exactMatches = cards
+            .reduce((list, current) => {
+              const foundIndex = list.findIndex(n => n.name === current.name);
+              if (foundIndex === -1) {
+                return list.concat(current);
+              }
+              if (current.imageUrl) {
+                list[foundIndex] = current;
+              }
+              return list;
+            }, [])
+            .filter(card => {
+              if (card.name.toLowerCase() === name.toLowerCase()) return true;
+              const firstWord = card.name.split(" ")[0];
+              if (firstWord.includes(name) && !firstWord.includes("'s")) {
+                return true;
+              }
+              partialMatches.push(card);
+              return false;
             });
-        }
-      })
-    );
+          //matchingCards.map(c => console.log(c.name));
+          if (exactMatches.length === 0) {
+            if (partialMatches.length === 0) {
+              msg.reply(`I couldn't find a card named ${name}`);
+            } else {
+              pollForCorrectCard(name, msg, partialMatches);
+            }
+          } else if (exactMatches.length === 1) {
+            let card = exactMatches[0];
+            const embed = new Discord.MessageEmbed()
+              .setTitle(card.name)
+              .setImage(card.imageUrl);
+            msg.channel.send(embed);
+          } else {
+            pollForCorrectCard(name, msg, exactMatches);
+          }
+        })
+      )
+  );
 }
 
 /*----------------------------------------------------------------------------*/
 /* Helper functions
 /*----------------------------------------------------------------------------*/
+function pollForCorrectCard(name, msg, matchingCards) {
+  return msg.channel
+    .send(
+      `I found several cards matching ${name}. Which did you mean?\n${matchingCards
+        .reduce(
+          (list, card, idx) =>
+            list + `${discordEmojiForNumber(idx)} ${card.name}\n`,
+          ""
+        )
+        .slice(0, -1)}`
+    )
+    .then(pollMessage => {
+      const filter = (reaction, user) =>
+        emojisForMultipleCardResults.includes(reaction.emoji.name) &&
+        user.id !== pollMessage.author.id;
+      const collector = pollMessage.createReactionCollector(filter, {
+        max: 1,
+        time: 1000 * 60 * 5,
+      });
+      //console.log(matchingCards.length);
+      Promise.all(
+        matchingCards.map((x, idx) => {
+          const emoji = discordEmojiForNumber(idx);
+          return emoji ? pollMessage.react(emoji) : Promise.resolve();
+        })
+      ).catch(() => null);
+
+      collector
+        .on("collect", r => {
+          const idx = emojisForMultipleCardResults.findIndex(
+            item => item === r.emoji.name
+          );
+          const card = matchingCards[idx];
+          const embed = new Discord.MessageEmbed()
+            .setTitle(card.name)
+            .setImage(card.imageUrl);
+          msg.channel.send(embed);
+        })
+        .on("end", r => {
+          pollMessage.delete();
+        });
+    });
+}
 const emojisForMultipleCardResults = [
   "1️⃣",
   "2️⃣",
