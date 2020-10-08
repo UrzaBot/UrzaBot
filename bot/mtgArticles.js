ff3dcd4ca551244993f70dd2f7b854a2a997d9ce
David Horstman <davidzilla27@gmail.com>
2020-10-07 21:12:29 -0700

    Add comments and clean up

--- bot/mtgArticles.js
+++ bot/mtgArticles.js
@@ -1,26 +1,34 @@
 const DB = require("../data/articlesModel");
-const knex = require("../data/dbConfig");
 const { getParsedArticles } = require("../utils/requestNewsArticles");
 
 async function testArticles(msg) {
-  const { content, author } = msg;
+  const { content, author, guild } = msg;
   if (author.bot) return;
   const isTestMessage = content.match(/test/gi);
   if (!isTestMessage) return;
+  console.log(guild)
   const newArticles = await getNewArticles();
+  newArticles.map(article => {
+    console.log(article);
+    // DB.addArticle(article).then(console.log);
+  });
 }
 
 function getNewArticles() {
+  // Get all new articles
   return getParsedArticles()
     .then(articles =>
       Promise.all(
         articles.map(article =>
+          // Check the database to see if the article is already in it
           DB.getArticleByUrl(article.url).then(articleInDB =>
+            // If the article is new, return it, otherwise return null
             !articleInDB ? article : null
           )
         )
       )
     )
+    // Remove all the nulls
     .then(results => results.filter(x => x));
 }
 
