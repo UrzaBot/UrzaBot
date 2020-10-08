92e28aa3c7c0de30a489102ca9d30b47d44b4909
David Horstman <davidzilla27@gmail.com>
2020-10-07 21:38:06 -0700

    Add status to bot

--- bot/bot.js
+++ bot/bot.js
@@ -5,6 +5,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("!help | github.com/urzabot");
   // setInterval(
   //   () => mtgArticles({ author: { bot: false }, content: "test" }),
   //   5000
